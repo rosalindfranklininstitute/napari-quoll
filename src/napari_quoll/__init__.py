c001c7350d18ef39ed1c93b622ab6879761979e9
Elaine Ho <elaine.ho@rfi.ac.uk>
2022-08-23 18:47:36 +0100

    First pass as napari-quoll

--- src/napari_quoll/__init__.py
+++ src/napari_quoll/__init__.py
@@ -1,15 +1,7 @@
 __version__ = "0.0.1"
 
-from ._reader import napari_get_reader
-from ._sample_data import make_sample_data
-from ._widget import ExampleQWidget, example_magic_widget
-from ._writer import write_multiple, write_single_image
+from ._widget import quoll_oneimgfrc
 
 __all__ = (
-    "napari_get_reader",
-    "write_single_image",
-    "write_multiple",
-    "make_sample_data",
-    "ExampleQWidget",
-    "example_magic_widget",
+    "quoll_oneimgfrc",
 )
--- src/napari_quoll/_widget.py
+++ src/napari_quoll/_widget.py
@@ -6,41 +6,54 @@ see: https://napari.org/stable/plugins/guides.html?#widgets
 
 Replace code below according to your needs.
 """
-from typing import TYPE_CHECKING
+from typing import Optional
 
 from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
 
-if TYPE_CHECKING:
-    import napari
+import napari
+from napari.utils.notifications import show_info
+import tempfile
 
-
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
-
+from quoll.io import reader as quollreader
+from quoll.frc import oneimg
+import os
+import numpy as np
 
 @magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+def quoll_oneimgfrc(
+    filename: str,
+    pixel_size: float,
+    unit: Optional[str] = "nm",
+    tile_size: Optional[int] = 256,
+):
+    QuollImg = quollreader.Image(
+        filename,
+        pixel_size,
+        unit,
+    )
+    
+    show_info(f"Working with image: {os.path.basename(filename)}")
 
+    viewer = napari.current_viewer()
+    viewer.add_image(QuollImg.img_data)
 
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+    tempdir = tempfile.TemporaryDirectory()
+
+    results_df = oneimg.calc_local_frc(
+        QuollImg,
+        tile_size=tile_size,
+        tiles_dir=tempdir.name
+    )
+
+    show_info(f"Tiles directory is {tempdir}")
+
+    show_info(f"Resolution is {np.mean(results_df.Resolution)}")
+
+    resolution_heatmap = oneimg.plot_resolution_heatmap(
+        QuollImg,
+        results_df,
+    )
+
+    viewer.add_image(resolution_heatmap, colormap="viridis", opacity=0.3)
+
+    tempdir.cleanup()
